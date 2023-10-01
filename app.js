1e9200c5947c73aeb22f94dec9b060e297a9885c
Smith Ringtho <sringtho@gmail.com>
2023-10-01 16:28:57 +0300

    Fix: Express rate limit path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connectDB = require('./db/connectDB')
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limiter')
+const rateLimiter = require('express-rate-limit')
 
 // Routers
 const authRouter = require('./routes/users')
@@ -19,9 +19,20 @@ const authenticateUser = require('./middleware/authentication')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const notFoundMiddleware = require('./middleware/not-found')
 
-app.use(express.json())
+// Extra security Middlewares
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+)
 app.use(cors())
 app.use(helmet())
+app.use(xss())
+
+app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/items', authenticateUser, itemsRouter)
