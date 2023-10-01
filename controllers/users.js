a44d0b90be2d1b32f29dd1be98a5abfce3fdd3d2
Smith Ringtho <sringtho@gmail.com>
2023-10-01 15:08:19 +0300

    Added update user and get user profile endpoints

--- controllers/users.js
+++ controllers/users.js
@@ -21,14 +21,36 @@ const login = async (req, res) => {
 }
 
 const signup = async (req, res) => {
-  const user = await User.create(req.body)
+  const user = (await User.create(req.body)).select(
+    '-password'
+  )
   const { first_name, last_name, email } = user
   res.status(StatusCodes.CREATED).json({ user: 
     { first_name, last_name, email } 
   })
 }
 
+const getUser = async (req, res) => {
+  const { userId } = req.user
+  const user = await User.findOne({ _id: userId }).select(
+    'first_name last_name email')
+  res.status(StatusCodes.OK).json({ user })
+}
+
+const updateUser = async (req, res) => {
+  const { userId } = req.user
+  const { first_name, last_name, password } = req.body
+  const user = await User.findOneAndUpdate(
+    { _id: userId },
+    { first_name, last_name, password },
+    { new: true, runValidators: true }
+  ).select('-password -_id')
+  res.status(StatusCodes.OK).json({ user })
+}
+
 module.exports = {
     login,
-    signup
+    signup,
+    getUser,
+    updateUser
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { login, signup } = require('../controllers/users')
+const authenticateUser = require('../middleware/authentication')
+
+const { login, signup, getUser, updateUser } = require('../controllers/users')
 
 router.route('/login').post(login)
 router.route('/signup').post(signup)
+router.route('/profile')
+    .get(authenticateUser, getUser)
+    .patch(authenticateUser, updateUser)
 
 module.exports = router
